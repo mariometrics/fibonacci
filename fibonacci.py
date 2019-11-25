1cad96341e3d7104d2f735bc82bba2e6531127bf
mario <58108507+mariometrics@users.noreply.github.com>
2019-11-25 13:07:06 +0100

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+import numpy as np
+def fibonacci(n):
+    if n == 0:
+        return (0)
+    elif n == 1:
+        return 0,1
+    else:
+        fib = np.zeros(n)
+        fib[0] = 0
+        fib[1] = 1
+        for i in range(0,n-2):
+            fib[i+2] = fib[i+1] + fib[i]
+        for k in range(0,len(fib)):
+            print(fib[k])
+
+fibonacci(20)
